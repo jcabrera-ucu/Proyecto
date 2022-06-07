9e13f2e05e61a14f82e685f35ba44d8d43b04e45
Federico Ferreira <FEDERICO.FERREIRAF@correo.ucu.edu.uy>
2022-06-07 20:02:51 -0300

    Jugador y Estadistica V1 Añadidos

---
+++ src/Library/Estadistica.cs
@@ -0,0 +1,11 @@
+namespace Library;
+
+public class Estadistica
+{
+    public int Victorias = 0;
+    public int Derrotas = 0;
+    public int Aciertos = 0;
+    public int Fallos = 0;
+    public int Hundidos = 0;
+    public Estadistica(){}
+}
