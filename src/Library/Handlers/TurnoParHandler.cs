88fa1fae4acf3523063681cc288849363bbeb4c6
Federico Ferreira <FEDERICO.FERREIRAF@correo.ucu.edu.uy>
2022-06-09 20:40:48 -0300

    Primer Draft de handlers

---
+++ src/Library/Handlers/TurnoParHandler.cs
@@ -0,0 +1,36 @@
+namespace Library
+{
+    /// <summary>
+    /// Un "handler" del patrón Chain of Responsibility que implementa el comando "chau".
+    /// </summary>
+    public class TurnoParHandler : BaseHandler
+    {
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="TurnoParHandler"/>. Esta clase procesa el mensaje "chau"
+        /// y el mensaje "adiós" -un ejemplo de cómo un "handler" puede procesar comandos con sinónimos.
+        /// </summary>
+        /// <param name="next">El próximo "handler".</param>
+        public TurnoParHandler(BaseHandler next) : base(next)
+        {
+            this.Keywords = new string[] { "chau", "adiós" };
+        }
+
+        /// <summary>
+        /// Procesa el mensaje "chau" y retorna true; retorna false en caso contrario.
+        /// </summary>
+        /// <param name="message">El mensaje a procesar.</param>
+        /// <param name="response">La respuesta al mensaje procesado.</param>
+        /// <returns>true si el mensaje fue procesado; false en caso contrario.</returns>
+        protected override bool InternalHandle(Message message, out string response)
+        {
+            if (this.CanHandle(message))
+            {
+                response = "a";
+                return true;
+            }
+
+            response = string.Empty;
+            return false;
+        }
+    }
+}
