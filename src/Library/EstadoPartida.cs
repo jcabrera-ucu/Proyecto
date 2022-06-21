200013359cecb563809857679cbb96d1ff8eb2b5
NicolasAbreuUCU <101676516+NicolasAbreuUCU@users.noreply.github.com>
2022-06-21 14:08:48 -0300

    Documentación para EstadoPartida

--- src/Library/EstadoPartida.cs
+++ src/Library/EstadoPartida.cs
@@ -1,19 +1,22 @@
 namespace Library;
 
 /// <summary>
-///
+/// Los distintos estados posibles de la Batalla Naval.
 /// </summary>
 public enum EstadoPartida
 {
-    /// <summary> </summary>
+    /// <summary>Los jugadors deben configurar sus barcos</summary>
     Configuración,
 
-    /// <summary> </summary>
+    /// <summary>Es el turno del Jugador A</summary>
     TurnoJugadorA,
 
-    /// <summary> </summary>
+    /// <summary>Es el turno del Jugador B</summary>
     TurnoJugadorB,
 
-    /// <summary> </summary>
+    /// <summary>La partida ha terminado por hundimiento de barcos</summary>
     Terminado,
+
+    /// <summary>La partida ha terminado por tiempo</summary>
+    TerminadoPorReloj,
 }
