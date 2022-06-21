582669bcb7035c21c27f14bae2a1513f9d067ef6
BenjaminRielli <101676520+BenjaminRielli@users.noreply.github.com>
2022-06-21 14:22:40 -0300

    Se agrega documentación en Estadistica

--- src/Library/Estadistica.cs
+++ src/Library/Estadistica.cs
@@ -1,11 +1,37 @@
 namespace Library;
 
+/// <summary>
+/// Estadísticas globales de un jugador
+/// </summary>
 public class Estadistica
 {
-    public int Victorias = 0;
-    public int Derrotas = 0;
-    public int Aciertos = 0;
-    public int Fallos = 0;
-    public int Hundidos = 0;
-    public Estadistica(){}
+    /// <summary>
+    /// Cantidad de victorias acumuladas
+    /// </summary>
+    public int Victorias { get; set; } = 0;
+
+    /// <summary>
+    /// Cantidad de derrotas acumuladas
+    /// </summary>
+    public int Derrotas { get; set; } = 0;
+
+    /// <summary>
+    /// Cantidad de ataques exitosos
+    /// </summary>
+    public int Aciertos { get; set; } = 0;
+
+    /// <summary>
+    /// Número de ataques que no golpean ningún barco
+    /// </summary>
+    public int Fallos { get; set; } = 0;
+
+    /// <summary>
+    /// Cantidad de total de barcos hundidos
+    /// </summary>
+    public int Hundidos { get; set; } = 0;
+
+    /// <summary>
+    /// Cantidad de veces que se usaron radares
+    /// </summary>
+    public int Radares { get; set; } = 0;
 }
