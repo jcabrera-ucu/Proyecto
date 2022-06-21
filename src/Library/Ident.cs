759464e419a2bbe717ae578151b9355c273b7578
NicolasAbreuUCU <101676516+NicolasAbreuUCU@users.noreply.github.com>
2022-06-21 14:09:16 -0300

    Clase Ident

---
+++ src/Library/Ident.cs
@@ -0,0 +1,28 @@
+namespace Library;
+
+/// <summary>
+/// Representa un identificador en formato string
+/// </summary>
+public record struct Ident
+{
+    /// <summary>
+    /// El valor interno del identificador
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    /// Construye un Id con un identificador aleatorio (Guid)
+    /// </summary>
+    public Ident()
+    {
+        Value = Guid.NewGuid().ToString();
+    }
+
+    /// <summary>
+    /// Construye un Id con un identificador dado
+    /// </summary>
+    public Ident(string id)
+    {
+        Value = id;
+    }
+}
