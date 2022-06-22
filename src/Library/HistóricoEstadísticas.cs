be68ec936977db4f676232aa375ecfac92b32547
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2022-06-21 22:42:21 -0300

    Tests de estadísticas

--- src/Library/HistóricoEstadísticas.cs
+++ src/Library/HistóricoEstadísticas.cs
@@ -3,16 +3,32 @@ using System.Text.Json;
 namespace Library;
 
 // SRP, su unica responsabilidad es persistir las estadisticas.
+/// <summary>
+/// Maneja la persistencia de las estadísticas de los usuarios
+/// </summary>
 public class HistóricoEstadísticas
 {
+    /// <summary>
+    /// Asociación entre ids de usuarios y su estadística
+    /// </summary>
     public Dictionary<string, Estadistica> Estadísticas { get; private set; }
 
+    /// <summary>
+    /// Ruta al archivo donde se guardan los datos
+    /// </summary>
     public string RutaArchivo { get; }
 
+    /// <summary>
+    /// Construye el gestor con ruta "bdd.json"
+    /// </summary>
     public HistóricoEstadísticas() : this("bdd.json")
     {
     }
 
+    /// <summary>
+    /// Construye el gestor con una ruta de archivo dada
+    /// </summary>
+    /// <param name="rutaArchivo">Ruta al archivo de datos</param>
     public HistóricoEstadísticas(string rutaArchivo)
     {
         RutaArchivo = rutaArchivo;
@@ -21,6 +37,11 @@ public class HistóricoEstadísticas
         Cargar();
     }
 
+    /// <summary>
+    /// Retorna las estadísticas de un usuario
+    /// </summary>
+    /// <param name="id">Id del usuario</param>
+    /// <returns>Instancia de estadística del usuario</returns>
     public Estadistica ObtenerEstadística(Ident id)
     {
         if (!Estadísticas.ContainsKey(id.Value))
@@ -31,11 +52,17 @@ public class HistóricoEstadísticas
         return Estadísticas[id.Value];
     }
 
+    /// <summary>
+    /// Guarda el histórico en el archivo
+    /// </summary>
     public void Guardar()
     {
         File.WriteAllText(RutaArchivo, JsonSerializer.Serialize(Estadísticas));
     }
 
+    /// <summary>
+    /// Carga el histórico desde el archivo
+    /// </summary>
     public void Cargar()
     {
         var json = CargarJson();
@@ -48,6 +75,10 @@ public class HistóricoEstadísticas
         }
     }
 
+    /// <summary>
+    /// Lee el json del archivo
+    /// </summary>
+    /// <returns>El contenido del archivo, o un Json "vacío"</returns>
     private string CargarJson()
     {
         try
