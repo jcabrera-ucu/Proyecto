c080d5f496c69a48de0d6363d9f604adc2009130
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2022-06-21 22:25:09 -0300

    Comentarios en jugada

--- src/Library/Jugada.cs
+++ src/Library/Jugada.cs
@@ -1,32 +1,32 @@
 namespace Library;
-// Esta clase cumple con SRP, se encarga de procesar jugadas.
+
+// Esta clase cumple con SRP, se encarga de representar la información de una jugada
 /// <summary>
-///
+/// Contiene los datos para una jugada.
 /// </summary>
  public  class Jugada
  {
     /// <summary>
-    ///
+    /// Identificador del jugador que realiza la jugada
     /// </summary>
     public Ident Id { get; }
 
     /// <summary>
-    ///
+    /// El tipo de jugada que es (ataque o radar)
     /// </summary>
     public TipoJugada Tipo { get; }
 
     /// <summary>
-    ///
+    /// La coordenada sobre la cual se realiza la jugada
     /// </summary>
-    /// <value></value>
     public Coord Coordenada { get; }
 
     /// <summary>
-    ///
+    /// Construye una jugada
     /// </summary>
-    /// <param name="id"></param>
-    /// <param name="tipo"></param>
-    /// <param name="coord"></param>
+    /// <param name="id">Id del jugador</param>
+    /// <param name="tipo">Tipo de jugada</param>
+    /// <param name="coord">Coordenada de destino</param>
     public Jugada(Ident id, TipoJugada tipo, Coord coord)
     {
         Id = id;
