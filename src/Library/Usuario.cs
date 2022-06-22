d60cd0e60d6029bd6e123c771df37de82772816a
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2022-06-21 22:27:54 -0300

    Comentarios en Usuario

--- src/Library/Usuario.cs
+++ src/Library/Usuario.cs
@@ -1,19 +1,22 @@
 namespace Library;
 // Esta clase cumple con SRP, su unica responsabilidad es mantener ciertos atributos de los usuarios.
 /// <summary>
-/// Creamos la clase Usuario para poder guardar la Id de cada usuario en la clase ListaUsuario,
-/// La Id la utilizamos para conectar los handlet del telegram con el programa.
+/// Información de usuario
 /// </summary>
 public class Usuario
 {
     /// <summary>
-    /// Guardamo la Id como un string para poder manejarla mejor más adelante.
-    /// La Id es pasada por parametro desde Telegram en el constructor de Usuario con el fin de que cada usurio tenga una Id única.
+    /// Identificador único del usuario (se va a usar en Jugador también)
     /// </summary>
-    /// <value>Valor de la Id obtenida de telegram.</value>
     public Ident Id { get; set; }
 
+    /// <summary>
+    /// Nombre del usuario
+    /// </summary>
     public string Nombre { get; set; } = String.Empty;
 
+    /// <summary>
+    /// Las estadísticas de juego de este usuario
+    /// </summary>
     public Estadistica Estadisticas { get; set; } = new();
 }
